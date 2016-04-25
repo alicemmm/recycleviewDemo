5305e5981f3500d5fe84a5e24362357dce46c6e4
alicemmm <345646798@qq.com>
2016-04-25 15:42:29 +0800

    add SimpleBaseAdapter for listview

---
+++ app/src/main/java/com/perasia/recycleviewdemo/SimpleBaseAdapter.java
@@ -0,0 +1,95 @@
+package com.perasia.recycleviewdemo;
+
+import android.content.Context;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class SimpleBaseAdapter<T> extends BaseAdapter {
+
+    protected Context mContext;
+    protected List<T> mData;
+
+    public SimpleBaseAdapter(Context context, List<T> data) {
+        this.mContext = context;
+        this.mData = data == null ? new ArrayList<T>() : new ArrayList<>(data);
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if (position >= mData.size())
+            return null;
+        return mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public abstract int getItemResource();
+
+    public abstract View getItemView(int position, View convertView, ViewHolder holder);
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = View.inflate(mContext, getItemResource(), null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        return getItemView(position, convertView, holder);
+    }
+
+    public void addAll(List<T> elem) {
+        mData.addAll(elem);
+        notifyDataSetChanged();
+    }
+
+    public void remove(T elem) {
+        mData.remove(elem);
+        notifyDataSetChanged();
+    }
+
+    public void remove(int index) {
+        mData.remove(index);
+        notifyDataSetChanged();
+    }
+
+    public void replaceAll(List<T> elem) {
+        mData.clear();
+        mData.addAll(elem);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder {
+        private SparseArray<View> views = new SparseArray<View>();
+        private View convertView;
+
+        public ViewHolder(View convertView) {
+            this.convertView = convertView;
+        }
+
+        public <T extends View> T getView(int resId) {
+            View v = views.get(resId);
+            if (v == null) {
+                v = convertView.findViewById(resId);
+                views.put(resId, v);
+            }
+            return (T) v;
+        }
+    }
+}
